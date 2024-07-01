7383ad268a194eddb6cad05ff62899ab8c36cb5c
rsduran <reineirsamonteduran@gmail.com>
2024-07-01 22:44:42 +0800

    Add key pair to EC2 instance configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "quiz_app" {
   ami           = "ami-0d2f6341ccecfd1b2"
   instance_type = "t2.micro"
+  key_name      = "quiz-app-key-pair"  # Ensure this matches the key pair name in AWS
 
   tags = {
     Name = "QuizAppInstance"
