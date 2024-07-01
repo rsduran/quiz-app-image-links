21908ebf4c8afa3177635765ec6005470970043a
rsduran <reineirsamonteduran@gmail.com>
2024-07-01 20:59:07 +0800

    Add terraform directory after cleaning

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "quiz_app" {
-  ami           = "ami-0d2f6341ccecfd1b2"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "QuizAppInstance"
-  }
-}
--- terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_access_key = "AKIATCKATZGOTLZ7GUOI"
-aws_secret_key = "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"
-aws_region     = "ap-southeast-2"
