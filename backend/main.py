85e253a1b81f53d3254ef60a2500e960209cf0d3
rsduran <reineirsamonteduran@gmail.com>
2024-09-13 06:00:55 -0700

    Updated files

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from app_init import app  # Import directly
+from app_init import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
