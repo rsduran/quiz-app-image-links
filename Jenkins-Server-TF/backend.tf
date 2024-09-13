bd881a27bd4a9b9461d794d98ea7e97171ce76c8
rsduran <reineirsamonteduran@gmail.com>
2024-09-12 21:56:46 -0700

    Latest commit

---
+++ Jenkins-Server-TF/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "athena-quiz-app-bucket"
+    region         = "ap-southeast-2"
+    key            = "quiz-app/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
