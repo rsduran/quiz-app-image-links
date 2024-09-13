1c2a688ebcc9e19dcd255d936d258abf51f43ab9
rsduran <reineirsamonteduran@gmail.com>
2024-09-13 08:00:20 -0700

    Updated files

--- frontend/pages/api/scrape.ts
+++ frontend/pages/api/scrape.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       // You might want to validate the input here
-      const response = await fetch(`http://k8s-threetie-mainlb-b5b9250791-1739950720.ap-southeast-2.elb.amazonaws.com/api/startScraping`, {
+      const response = await fetch('http://k8s-threetie-mainlb-b5b9250791-1739950720.ap-southeast-2.elb.amazonaws.com/api/startScraping', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body),
