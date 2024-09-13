33293fab149331e602264fa5044d20375644bd43
rsduran <reineirsamonteduran@gmail.com>
2024-09-13 09:54:15 -0700

    Updated files

--- frontend/components/CalendarEditor.tsx
+++ frontend/components/CalendarEditor.tsx
@@ -45,7 +45,7 @@ const CalendarEditor = () => {
   }, [date]);
 
   const fetchEditorContent = () => {
-    fetch(`http://a9184e9e7d5f94758bf1670e8fb3fa88-841219625.ap-southeast-2.elb.amazonaws.com/api/getEditorContent`)
+    fetch(`http://k8s-threetie-backends-53ff77a1f2-63fc387682774c76.elb.ap-southeast-2.amazonaws.com/api/getEditorContent`)
       .then(response => response.json())
       .then(data => {
         setEditorContent(data.content);
@@ -58,7 +58,7 @@ const CalendarEditor = () => {
     setEditorContent(content);
     localStorage.setItem('editorContent', content);
 
-    fetch(`http://a9184e9e7d5f94758bf1670e8fb3fa88-841219625.ap-southeast-2.elb.amazonaws.com/api/saveEditorContent`, {
+    fetch(`http://k8s-threetie-backends-53ff77a1f2-63fc387682774c76.elb.ap-southeast-2.amazonaws.com/api/saveEditorContent`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ content }),
--- frontend/pages/api/scrape.ts
+++ frontend/pages/api/scrape.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       // You might want to validate the input here
-      const response = await fetch(`http://a9184e9e7d5f94758bf1670e8fb3fa88-841219625.ap-southeast-2.elb.amazonaws.com/api/startScraping`, {
+      const response = await fetch(`http://k8s-threetie-backends-53ff77a1f2-63fc387682774c76.elb.ap-southeast-2.amazonaws.com/api/startScraping`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body),
