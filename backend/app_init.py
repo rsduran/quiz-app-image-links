8b79984625c139a0fae30bedaaefb9a18909d694
rsduran <reineirsamonteduran@gmail.com>
2024-09-14 03:24:21 -0700

    Updated files

--- backend/app_init.py
+++ backend/app_init.py
@@ -1,21 +1,17 @@
 # app_init.py
 
 import os
-import logging
 from flask import Flask
 from flask_cors import CORS
 from db import db
 
 app = Flask(__name__)
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 # Secret key for session management
 app.secret_key = '3c6e0b8a9c15224a8228b9a98ca1531d'
 
 # Configure PostgreSQL database URI
+# Change 'localhost' to 'db' to point to the Docker service name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://my_user:password@postgres:5432/quizdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
