9559f88396040481ecd69ad2fb53f8e195cf308b
rsduran <reineirsamonteduran@gmail.com>
2024-09-14 21:46:30 -0700

    Updated files

--- backend/app_init.py
+++ backend/app_init.py
@@ -11,7 +11,7 @@ app.secret_key = '3c6e0b8a9c15224a8228b9a98ca1531d'
 # Configure PostgreSQL database URI using environment variables
 DB_HOST = os.getenv('DB_HOST', 'postgres')
 DB_NAME = os.getenv('DB_NAME', 'quizdb')
-DB_USER = os.getenv('DB_USER', 'user')
+DB_USER = os.getenv('DB_USER', 'my_user')
 DB_PASS = os.getenv('DB_PASS', 'password')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
